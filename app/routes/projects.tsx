fe840c8fdb4ebefa1b7070c400e559c55c41a5ed
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2024-04-24 16:14:50 +0200

    add up and down buttons

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,7 +1,8 @@
-import { useRef, useState } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
 import ContactIcons from "~/components/contactIcons";
-import paidProjectsImport from "~/projects/paid.json";
-import unpaidProjectsImport from "~/projects/unpaid.json";
+import paidProjects from "~/projects/paid.json";
+import unpaidProjects from "~/projects/unpaid.json";
 import { calculateMonthsDifference } from "~/utils";
 
 type Project = {
@@ -17,63 +18,96 @@ type Project = {
   stack: string[];
 };
 
+function highlightWords(title: string, list: string) {
+  const splittedList = list.split(",").map((word: string) => word.trim());
+  const highlightedString = title.split(" ").map((word) => {
+    return splittedList.includes(word) ? `<mark>${word}</mark>` : word;
+  });
+  return <div>{highlightedString.join(" ")}</div>;
+}
+
+const firstScreenId = "firstScreen";
+const radioProfessionalId = "radio-professional";
+const radioPrivateId = "radio-private";
+
 export default function ProjectsPage() {
-  const [selectedOption, setSelectedOption] = useState<
-    "professional" | "private" | "initial"
-  >("initial");
+  const [selection, setSelection] = useState<
+    "professional" | "private" | "none"
+  >("none");
   const [projects, setProjects] = useState<Project[]>([]);
 
-  const handleRadioChange = (selection: "professional" | "private") => {
-    setSelectedOption(selection);
+  useEffect(() => {
+    if (selection == "none") return;
 
-    setProjects(
-      (selection == "professional"
-        ? paidProjectsImport
-        : unpaidProjectsImport
-      ).map((project) => {
-        return {
-          ...project,
-          duration: calculateMonthsDifference(project.from, project.until),
-          titleWithHighlights: highlightWords(project.title, project.highlight),
-        };
-      })
-    );
+    document.getElementById(radioProfessionalId)?.blur();
+    document.getElementById(radioPrivateId)?.blur();
+
+    const projectsToUpdate = (
+      selection === "professional" ? paidProjects : unpaidProjects
+    ).map((project) => ({
+      ...project,
+      duration: calculateMonthsDifference(project.from, project.until),
+      titleWithHighlights: highlightWords(project.title, project.highlight),
+    }));
+
+    setProjects(projectsToUpdate);
+  }, [selection]);
+
+  useEffect(() => {
+    scrollIntoView("project0");
+  }, [projects]);
+
+  const handleReset = () => {
+    setSelection("none");
+    setProjects([]);
+    scrollIntoView(firstScreenId);
   };
 
-  function highlightWords(title: string, list: string) {
-    const splittedList = list.split(",").map((word: string) => word.trim());
-    const highlightedString = title.split(" ").map((word) => {
-      return splittedList.includes(word) ? `<mark>${word}</mark>` : word;
-    });
-    return <div>{highlightedString.join(" ")}</div>;
-  }
+  const handleSwipeUp = (e: any) => {
+    const i = parseInt(e.currentTarget.getAttribute("data-value"));
+    if (i == 0) {
+      scrollIntoView(firstScreenId);
+    } else {
+      scrollIntoView(`project${i - 1}`);
+    }
+  };
 
-  const firstScreenRef = useRef<HTMLDivElement | null>(null);
+  const handleSwipeDown = (e: any) => {
+    const i = parseInt(e.currentTarget.getAttribute("data-value"));
+    if (i < projects.length - 1) {
+      scrollIntoView(`project${i + 1}`);
+    }
+  };
+
+  function scrollIntoView(id: string) {
+    const screen = document.getElementById(id);
+    if (screen) {
+      screen.scrollIntoView({ behavior: "smooth" });
+    }
+  }
 
   return (
     <div className="container">
-      {/* first screen */}
+      {/* selection screen */}
       <div
+        id={firstScreenId}
         className="tiktok p-6 flex flex-col gap-8 justify-center items-start"
-        ref={firstScreenRef}
       >
         <div>
-          My past <mark>projects</mark> in a TikTok-like feed 📺
+          <mark>Swipe</mark> through my <mark>projects</mark> in a TikTok-like
+          feed 📺
         </div>
-        <div>
-          {"> "}Unlike TikTok, <mark>choose</mark> what you want to see here
-        </div>
+        <div>{"> "}Unlike TikTok, choose what you want to see here</div>
         <div className="flex items-center justify-center">
           <input
             type="radio"
             value="professional"
-            name="default-radio"
             className="w-4 h-4 accent-[#ffe100]"
-            id="default-radio-1"
-            checked={selectedOption === "professional"}
-            onChange={() => handleRadioChange("professional")}
+            id={radioProfessionalId}
+            checked={selection === "professional"}
+            onChange={() => setSelection("professional")}
           />
-          <label htmlFor="default-radio-1" className="ms-2">
+          <label htmlFor={radioProfessionalId} className="ms-2">
             Professional projects
           </label>
         </div>
@@ -81,29 +115,23 @@ export default function ProjectsPage() {
           <input
             type="radio"
             value="private"
-            name="default-radio"
             className="w-4 h-4 accent-[#ffe100]"
-            id="default-radio-2"
-            checked={selectedOption === "private"}
-            onChange={() => handleRadioChange("private")}
+            id={radioPrivateId}
+            checked={selection === "private"}
+            onChange={() => setSelection("private")}
           />
-          <label htmlFor="default-radio-2" className="ms-2">
+          <label htmlFor={radioPrivateId} className="ms-2">
             Private projects
           </label>
-        </div>
-        <div
-          hidden={selectedOption != "initial"}
-          className="text-white [text-shadow:_0_0_0_rgb(0_0_0_/_0%)]"
-        >
-          thisIsHidden
-        </div>
-        <div hidden={selectedOption == "initial"}>
-          ⬇️ <mark>swipe</mark> down ⬇️
         </div>
       </div>
       {/* projects */}
       {projects.map((project, i) => (
-        <div className="tiktok p-6 flex flex-col gap-4" key={project.title}>
+        <div
+          id={`project${i}`}
+          className="tiktok p-6 flex flex-col gap-4"
+          key={project.title}
+        >
           <div>
             {"> "}
             <span
@@ -159,14 +187,20 @@ export default function ProjectsPage() {
           {/* page number */}
           <div className="mt-auto ml-auto text-sm lg:text-xl">
             <button
-              className="py-1 px-2 mr-2 rounded bg-gray-300"
-              onClick={() => {
-                if (firstScreenRef.current) {
-                  setSelectedOption("initial");
-                  firstScreenRef.current.scrollIntoView({ behavior: "smooth" });
-                }
-              }}
+              className="py-1 px-2 mr-2"
+              data-value={i}
+              onClick={handleSwipeUp}
             >
+              ⬆️
+            </button>
+            <button
+              className="py-1 px-2 mr-2"
+              data-value={i}
+              onClick={handleSwipeDown}
+            >
+              ⬇️
+            </button>
+            <button className="py-1 px-2 mr-2" onClick={handleReset}>
               Reset
             </button>
             {i + 1}/{projects.length}
